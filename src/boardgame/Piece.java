55c4dd9c09f0d5a8ee4411e685994fcfc522ff0c
Diogo Batista <diogobatista330@gmail.com>
2024-04-17 06:31:44 -0300

    implement board and piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,16 @@
+
+package boardgame;
+
+public class Piece {
+	private Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+	
+	protected Board getBoard() {
+		return board;
+	}
+}
