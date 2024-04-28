d24d6e8e935c8ac6d7afbb4e85ae2538e629dfcb
Diogo Batista <diogobatista330@gmail.com>
2024-04-28 13:33:41 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -56,6 +56,10 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println("Turn: " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		
+		if(chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
